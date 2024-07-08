bb92a7da44a5c1a8e94be1b6bdcdf2f89b7d2f9a
邓锐豪 <dangyeuihou@icloud.com>
2024-07-08 11:37:01 +0800

    修改文件名

--- vue-project/src/router/index.js
+++ vue-project/src/router/index.js
@@ -1,8 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-import AccessCheckView from '../views/AccessCheckView1.vue'
+import AccessCheckView from '../views/AccessCheckView.vue'
 import InvokeFlowView from '../views/InvokeFlowView.vue'
-import MetadataBrowser from '../views/MetadataBrowser.vue'
+import MetadataBrowser from '../views/MetadataBrowserView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
